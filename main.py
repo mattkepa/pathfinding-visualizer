981560b841cfc91f55419fe2313e1b266c068769
Mateusz Kępa <matt.kepa@gmail.com>
2023-03-01 18:00:05 +0100

    feat: add functionality to draw grid and nodes on it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from app_config import AppConfig
+from utils import draw, get_clicked_pos
 
 
 pygame.init()
@@ -14,9 +15,35 @@ def main():
 
     run = True
     while run:
+
+        draw(app.window, app.grid, app.rows, app.columns, app.size)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+            if pygame.mouse.get_pressed()[0]: # LEFT MOUSE CLICK
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, app.rows, app.columns, app.size)
+                clicked_node = app.grid[row][col]
+                if start_node == None and clicked_node != end_node and not clicked_node.is_obstacle():
+                    start_node = clicked_node
+                    clicked_node.make_start()
+                elif end_node == None and clicked_node != start_node and not clicked_node.is_obstacle():
+                    end_node = clicked_node
+                    clicked_node.make_end()
+                elif clicked_node != end_node and clicked_node != start_node:
+                    clicked_node.make_obstacle()
+
+            elif pygame.mouse.get_pressed()[2]: # RIGHT MOUSE CLICK
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, app.rows, app.columns, app.size)
+                clicked_node = app.grid[row][col]
+                clicked_node.reset()
+                if clicked_node == start_node:
+                    start_node = None
+                elif clicked_node == end_node:
+                    end_node = None
 
     pygame.quit()
 
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 # CONSTANTS
 COLORS = {
     'white': (255, 255, 255),
@@ -9,3 +12,57 @@ COLORS = {
     'teal': (48, 176, 199),
     'purple': (93, 47, 136)
 }
+
+
+
+def draw(surface, grid, rows, columns, size):
+    """
+    Draws grid lines and nodes on the screen and updates it
+
+    :param surface: Surface - pygame surface object
+    :param rows: int - number of rows of grid
+    :param columns: int - number of columns of grid
+    :param size: int - size of the width and height of screen in pixels
+    """
+    surface.fill(COLORS['white'])
+
+    for row in grid:
+        for node in row:
+            node.draw(surface)
+
+    draw_grid(surface, rows, columns, size)
+
+    pygame.display.update()
+
+
+def draw_grid(surface, rows, columns, size):
+    """
+    Draws grid lines on the screen
+
+    :param surface: Surface - pygame surface object
+    :param rows: int - number of rows of grid
+    :param columns: int - number of columns of grid
+    :param size: int - size of the width and height of screen in pixels
+    """
+    n_size = size // rows #set size of node based on window size and number of rows/columns
+    for i in range(rows):
+        pygame.draw.line(surface, COLORS['gray'], (0, i*n_size), (size, i*n_size))
+    for j in range(columns):
+        pygame.draw.line(surface, COLORS['gray'], (j*n_size, 0), (j*n_size, size))
+
+
+def get_clicked_pos(pos, rows, columns, size):
+    """
+    Returns tuple of row and column clicked node's
+
+    :param pos: tuple[int, int] - cursor position when click occured
+    :param rows: int - number of rows of grid
+    :param columns: int - number of columns of grid
+    :param size: int - size of the width and height of screen in pixels
+    """
+    n_size = size // rows #set size of node based on window size and number of rows/columns
+    y, x = pos
+
+    row =  y // n_size
+    col = x // n_size
+    return (row, col)
